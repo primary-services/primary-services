91f6abc999a33085e52a5359039ba1e528580a7e
Joel <>
2025-05-28 08:56:39 -0400

    Adding API route for loading offices by town name

--- src/node-api/src/controllers/office.controller.js
+++ src/node-api/src/controllers/office.controller.js
@@ -1,5 +1,13 @@
+import Municipality from "../models/municipality.model.js";
+import Election from "../models/election.model.js";
+import Official from "../models/official.model.js";
 import Office from "../models/office.model.js";
 import Seat from "../models/seat.model.js";
+import Term from "../models/term.model.js";
+
+import Requirement from "../models/requirement.model.js";
+import Deadline from "../models/deadline.model.js";
+import Form from "../models/form.model.js";
 
 import {
   BadRequestError,
@@ -8,6 +16,57 @@ import {
 } from "../utils/ApiError";
 
 let officeController = {
+  list: async (req, res, next) => {
+    try {
+      let { municipality_name } = req.params;
+      let sequelize = Municipality.sequelize;
+
+      let municipality = await Municipality.findOne({
+        where: sequelize.where(
+          sequelize.fn("lower", sequelize.col("name")),
+          sequelize.fn("lower", municipality_name),
+        ),
+      });
+
+      // Might want a view here
+      let offices = await Office.findAll({
+        where: { municipality_id: municipality.id, elected: true },
+        include: [
+          { model: Official, as: "officials" },
+          {
+            model: Seat,
+            as: "seats",
+            include: [
+              {
+                model: Term,
+                as: "terms",
+                include: [
+                  {
+                    model: Election,
+                    as: "elections",
+
+                    include: {
+                      model: Requirement,
+                      as: "requirements",
+                      include: [
+                        { model: Deadline, as: "deadline" },
+                        { model: Form, as: "form" },
+                      ],
+                    },
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      });
+
+      return res.status(200).json(offices);
+    } catch (error) {
+      next(error);
+    }
+  },
+
   save: async (req, res, next) => {
     try {
       let data = req.body;
--- src/node-api/src/routes/office.routes.js
+++ src/node-api/src/routes/office.routes.js
@@ -4,6 +4,7 @@ import officeController from "../controllers/office.controller.js";
 
 const officeRoutes = Router();
 
+officeRoutes.get("/offices/:municipality_name", officeController.list);
 officeRoutes.post("/office", officeController.save);
 
 export { officeRoutes };
