913bbc2d5c2ba2e60ce1d07d35abdf86162a7a29
Joel <>
2025-06-01 17:22:25 -0400

    Removing unnecessary call to old API

--- src/admin/src/pages/landing.js
+++ src/admin/src/pages/landing.js
@@ -1,20 +1,20 @@
 import { useState, useEffect } from "react";
 
 export const LandingPage = () => {
-  useEffect(() => {
-    (async () => {
-      const url =
-        "https://vy7ll9tz04.execute-api.us-east-2.amazonaws.com/dev/towns";
+  // useEffect(() => {
+  //   (async () => {
+  //     const url =
+  //       "https://vy7ll9tz04.execute-api.us-east-2.amazonaws.com/dev/towns";
 
-      const response = await fetch(url);
-      if (!response.ok) {
-        console.log(response);
-      } else {
-        const json = await response.json();
-        console.log(json);
-      }
-    })();
-  }, []);
+  //     const response = await fetch(url);
+  //     if (!response.ok) {
+  //       // console.log(response);
+  //     } else {
+  //       const json = await response.json();
+  //       // console.log(json);
+  //     }
+  //   })();
+  // }, []);
 
   return (
     <section id="landing-page" className="page">
