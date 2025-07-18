e477956a521a3b46ab158daddb61cd0cf687fe94
Joel <>
2025-07-18 13:15:36 -0400

    Separating out Root and the rest of the app

--- src/admin/src/App.js
+++ src/admin/src/App.js
@@ -1,53 +1,13 @@
 import "./scss/styles.scss";
 
-import { BrowserRouter, Routes, Route } from "react-router";
-
-import { LandingPage } from "./pages/landing.js";
-import { Towns } from "./pages/towns.js";
-
-import { SignupForm } from "./components/auth/signup.js";
-import { LoginForm } from "./components/auth/login.js";
+import { Loader } from "./Loader.js";
 
 import { AppProviders } from "./providers";
 
 function App() {
   return (
     <AppProviders>
-      <header>
-        <nav data-uk-navbar>
-          <div className="uk-navbar-left">
-            <ul className="uk-navbar-nav">
-              <li className="uk-active">
-                <a href="/" className="logo">
-                  <h2>Democracy Hub Admin</h2>
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div className="uk-navbar-right">
-            <div
-              className="btn blocky rev"
-              data-uk-toggle="target: #login-form"
-            >
-              Sign In
-            </div>
-          </div>
-        </nav>
-      </header>
-      <div className="content">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/towns" element={<Towns />} />
-            <Route path="/towns/ma/:slug" element={<Towns />} />
-          </Routes>
-        </BrowserRouter>
-      </div>
-
-      <div id="modals">
-        <SignupForm />
-        <LoginForm />
-      </div>
+      <Loader />
     </AppProviders>
   );
 }
