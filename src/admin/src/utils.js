5776778c2b88e08062c3d2eedcad95d3bde9878a
Joel <>
2025-07-18 13:19:07 -0400

    Moving these functions to the general utils

--- src/admin/src/utils.js
+++ src/admin/src/utils.js
@@ -1,46 +1,73 @@
 export const cleanDateString = (dateString) =>
   dateString ? dateString.replace(" 00:00:00 GMT", "") : dateString;
 
-// React 17 broke UI Kit modals. They're still worth using I think,
-// Just need this shim to open them
-export const openModal = (target) => {
-  console.log(target);
-  // let modal = document.querySelector(target);
-  // // let options = JSON.parse(modal.dataset["ukModal"].replace(/'/g, '"') || "");
-  // let current = document.querySelector(".uk-modal.uk-open");
+export const getCookie = (name) => {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
 
-  // if (current !== null) {
-  //   console.log(current);
-  //   current.dispatchEvent(new CustomEvent("close"));
-  // }
+  if (parts.length === 2) return parts.pop().split(";").shift();
+};
 
-  // modal.classList.add("uk-open");
-  // modal.style.display = "block";
+export const clearCookie = (name) => {
+  return window.cookieStore.delete(name);
+};
 
-  // let close = (e) => {
-  //   modal.dispatchEvent(new CustomEvent("close"));
-  // };
+export const validPassword = (password, confirmation) => {
+  let hasMinLen = false;
+  let hasUpper = false;
+  let hasLower = false;
+  let hasNumber = false;
+  let hasSpecial = false;
+  let hasConfirmation = false;
 
-  // let stop = (e) => {
-  //   e.stopPropagation();
-  // };
+  if (!!password && !!confirmation && password === confirmation) {
+    hasConfirmation = true;
+  }
 
-  // // if (options["bg-close"]) {
-  // modal.querySelector(".uk-modal-body").addEventListener("click", stop);
-  // modal.addEventListener("click", close);
-  // // }
+  if (password.length >= 8) {
+    hasMinLen = true;
+  }
 
-  // modal.querySelector(".uk-modal-close").addEventListener("click", close);
-  // modal.addEventListener("close", (e) => {
-  //   console.log("Closing:", e);
-  //   modal.classList.remove("uk-open");
+  if (/[!|@|#\$|%|\^|&|_]+/g.test(password)) {
+    hasSpecial = true;
+  }
 
-  //   setTimeout(() => {
-  //     modal.style.display = "none";
-  //   }, 100);
+  if (/[A-Z]+/g.test(password)) {
+    hasUpper = true;
+  }
 
-  //   modal.querySelector(".uk-modal-body").removeEventListener("click", stop);
-  //   modal.removeEventListener("click", close);
-  //   modal.querySelector(".uk-modal-close").removeEventListener("click", close);
-  // });
+  if (/[a-z]+/g.test(password)) {
+    hasLower = true;
+  }
+
+  if (/[0-9]+/g.test(password)) {
+    hasNumber = true;
+  }
+
+  return [
+    hasMinLen ? null : "length",
+    hasUpper ? null : "upper",
+    hasLower ? null : "lower",
+    hasNumber ? null : "number",
+    hasSpecial ? null : "special",
+    hasConfirmation ? null : "confirmation",
+  ].filter((e) => !!e);
+};
+
+export const checkRequired = (required, data) => {
+  let missing_fields = [];
+  required.map((field) => {
+    let path = field.split(".");
+
+    let obj = data;
+    for (var i = 0; i < path.length - 1; i++) {
+      obj = obj[path[i]];
+    }
+
+    if (!obj[path[path.length - 1]]) {
+      missing_fields.push(path[path.length - 1]);
+    }
+  });
+
+  return missing_fields;
 };
