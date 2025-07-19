2fc9b1988c9c9ec0291f5ea91651d5c72b59ebb7
Joel <>
2025-07-19 11:46:55 -0400

    Fixing spacing

--- src/admin/src/Loader.js
+++ src/admin/src/Loader.js
@@ -10,69 +10,69 @@ import { SignupPage } from "./pages/auth/signup.js";
 import { LoginPage } from "./pages/auth/login.js";
 
 export const Loader = () => {
-	const { loading, authorize, logout, user } = useContext(
-		AppContexts.AuthContext,
-	);
+  const { loading, authorize, logout, user } = useContext(
+    AppContexts.AuthContext,
+  );
 
-	useEffect(() => {
-		authorize();
-	}, []);
+  useEffect(() => {
+    authorize();
+  }, []);
 
-	return (
-		<>
-			{loading && <h1>We need a loading screen</h1>}
+  return (
+    <>
+      {loading && <h1>We need a loading screen</h1>}
 
-			{!loading && (
-				<BrowserRouter>
-					<header>
-						<nav data-uk-navbar>
-							<div className="uk-navbar-left">
-								<ul className="uk-navbar-nav">
-									<li className="uk-active">
-										<a href="/" className="logo">
-											<h2>Democracy Hub Admin</h2>
-										</a>
-									</li>
-								</ul>
-							</div>
-							<div className="uk-navbar-right">
-								{!!user ? (
-									<>
-										<div>
-											Welcome <a href="#">{user.email}</a>
-										</div>
-										<div>
-											<a href="#" onClick={logout}>
-												Logout
-											</a>
-										</div>
-									</>
-								) : (
-									<span>
-										<Link to="/login" className="btn blocky rev">
-											Sign In
-										</Link>
+      {!loading && (
+        <BrowserRouter>
+          <header>
+            <nav data-uk-navbar>
+              <div className="uk-navbar-left">
+                <ul className="uk-navbar-nav">
+                  <li className="uk-active">
+                    <a href="/" className="logo">
+                      <h2>Democracy Hub Admin</h2>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div className="uk-navbar-right">
+                {!!user ? (
+                  <>
+                    <div>
+                      Welcome <a href="#">{user.email}</a>
+                    </div>
+                    <div>
+                      <a href="#" onClick={logout}>
+                        Logout
+                      </a>
+                    </div>
+                  </>
+                ) : (
+                  <span>
+                    <Link to="/login" className="btn blocky rev">
+                      Sign In
+                    </Link>
 
-										<Link to="/signup" className="btn blocky rev">
-											Sign Up
-										</Link>
-									</span>
-								)}
-							</div>
-						</nav>
-					</header>
-					<div className="content">
-						<Routes>
-							<Route path="/login" element={<LoginPage />} />
-							<Route path="/signup" element={<SignupPage />} />
+                    <Link to="/signup" className="btn blocky rev">
+                      Sign Up
+                    </Link>
+                  </span>
+                )}
+              </div>
+            </nav>
+          </header>
+          <div className="content">
+            <Routes>
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/signup" element={<SignupPage />} />
 
-							<Route path="/" element={<LandingPage />} />
-							<Route path="/towns" element={<Towns />} />
-							<Route path="/towns/ma/:slug" element={<Towns />} />
-						</Routes>
-					</div>
-				</BrowserRouter>
-			)}
-		</>
-	);
+              <Route path="/" element={<LandingPage />} />
+              <Route path="/towns" element={<Towns />} />
+              <Route path="/towns/ma/:slug" element={<Towns />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      )}
+    </>
+  );
 };
