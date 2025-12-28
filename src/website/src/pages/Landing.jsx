fe9838c7398c24bf16d7627c99034bb576620f3c
Joel <>
2025-12-28 14:30:02 -0500

    Updating website landing page to include under construction message

--- src/website/src/pages/Landing.jsx
+++ src/website/src/pages/Landing.jsx
@@ -1,3 +1,13 @@
 export const Landing = ({ children }) => {
-	return <h1>Landing Page</h1>;
+	return (
+		<section>
+			<div style={{ width: "100vw", textAlign: "center" }}>
+				<h1>Under Construction</h1>
+				<p>
+					We're building public resources for information about Massachusetts
+					elections. Please check back later for more information
+				</p>
+			</div>
+		</section>
+	);
 };
