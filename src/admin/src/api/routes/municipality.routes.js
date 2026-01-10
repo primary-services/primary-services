f0b7017bd55e636da9deba88f549d0f6d62deeb0
Joel <>
2026-01-10 16:53:41 -0500

    Adding ward in the forms

--- src/admin/src/api/routes/municipality.routes.js
+++ src/admin/src/api/routes/municipality.routes.js
@@ -1,4 +1,5 @@
 import { apiRoot } from "../../constants.js";
+import { fetchWithAuth } from "../../utils.js";
 
 const routeMap = {
   municipality: "municipalities",
@@ -12,6 +13,29 @@ export const getCompletion = (args) => {
       "Content-Type": "application/json",
     },
   }).then((resp) => {
+    return resp.json();
+  });
+};
+
+export const createWard = (args) => {
+  return fetchWithAuth(
+    `${apiRoot}/municipalities/${args.municipality_id}/ward`,
+    {
+      method: "POST",
+      body: JSON.stringify({ ...args }),
+    },
+  ).then((resp) => {
+    return resp.json();
+  });
+};
+
+export const deleteWard = (args) => {
+  return fetchWithAuth(
+    `${apiRoot}/municipalities/${args.municipality_id}/ward/${args.id}`,
+    {
+      method: "DELETE",
+    },
+  ).then((resp) => {
     return resp.json();
   });
 };
