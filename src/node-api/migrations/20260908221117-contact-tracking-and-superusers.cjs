40fe1dadbf77bd4fa7ab402a38563cc7fb4df513
Maya Nigrin <36938499+mayigrin@users.noreply.github.com>
2026-09-08 18:25:56 -0400

    clean up migrations

--- src/node-api/migrations/20260908221117-contact-tracking-and-superusers.cjs
+++ src/node-api/migrations/20260908221117-contact-tracking-and-superusers.cjs
@@ -7,12 +7,14 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.addColumn("contact", "office_email", { type: "TEXT", allowNull: true, primaryKey: false, autoIncrement: false, })
     await queryInterface.addColumn("municipality", "contact_info_last_updated", { type: "TIMESTAMP WITH TIME ZONE", allowNull: true, primaryKey: false, autoIncrement: false, })
-    await queryInterface.addColumn("municipality", "clerk_office_provided_info", { type: "TIMESTAMP WITH TIME ZONE", allowNull: true, primaryKey: false, autoIncrement: false, })
+    await queryInterface.addColumn("municipality", "clerk_office_provided_info", { type: "BOOLEAN", allowNull: true, primaryKey: false, autoIncrement: false, })
+    await queryInterface.addColumn("user", "superuser", { type: "BOOLEAN", allowNull: true, primaryKey: false, autoIncrement: false, })
   },
 
   async down(queryInterface, Sequelize) {
     await queryInterface.removeColumn('contact', 'office_email', {})
     await queryInterface.removeColumn('municipality', 'contact_info_last_updated', {})
     await queryInterface.removeColumn('municipality', 'clerk_office_provided_info', {})
+    await queryInterface.removeColumn('user', 'superuser', {})
   },
 };
