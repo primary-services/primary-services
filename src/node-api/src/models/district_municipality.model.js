4f6ad718a63d6ff54078891b4cf6ff3914607d52
Maya Nigrin <36938499+mayigrin@users.noreply.github.com>
2026-09-08 23:24:13 -0400

    fix accidental file overwrite

--- src/node-api/src/models/district_municipality.model.js
+++ src/node-api/src/models/district_municipality.model.js
@@ -1,105 +1,54 @@
-"use strict";
+import { Sequelize, DataTypes } from "sequelize";
+import Model from "../lib/base-model.js";
 
-/** 
-    Generated file, please proof before running
-*/
-module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("district", {});
-    await queryInterface.createTable("district_municipalities", {});
-    await queryInterface.addColumn("district", "id", {
-      type: "INTEGER",
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    });
-    await queryInterface.addColumn("district", "name", {
-      type: "TEXT",
-      allowNull: true,
-      primaryKey: false,
-      autoIncrement: false,
-    });
-    await queryInterface.addColumn("district_municipalities", "id", {
-      type: "INTEGER",
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    });
-    await queryInterface.addColumn("district_municipalities", "district_id", {
-      type: "INTEGER",
-      allowNull: false,
-      primaryKey: false,
-      autoIncrement: false,
-    });
-    await queryInterface.addColumn(
-      "district_municipalities",
-      "municipality_id",
+class DistrictMunicipality extends Model {
+  static init(sequelize) {
+    super.init(
       {
-        type: "INTEGER",
-        allowNull: false,
-        primaryKey: false,
-        autoIncrement: false,
+        id: {
+          autoIncrement: true,
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+        },
+        district_id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+        },
+        municipality_id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        tableName: "district_municipalities",
+        schema: "public",
+        timestamps: false,
+        indexes: [
+          {
+            name: "district_municipalities_pkey",
+            unique: true,
+            fields: [{ name: "id" }],
+          },
+        ],
       },
     );
-    await queryInterface.addConstraint("district", {
-      fields: ["id"],
-      type: "primary key",
-      name: "district_pkey",
-    });
-    await queryInterface.addConstraint("district_municipalities", {
-      fields: ["id"],
-      type: "primary key",
-      name: "district_municipalities_pkey",
-    });
-    await queryInterface.addConstraint("district_municipalities", {
-      fields: ["district_id"],
-      type: "foreign key",
-      name: "district_municipalities_district_id_fkey",
-      references: { table: "district", field: "id" },
-      onDelete: "NO ACTION",
-      onUpdate: "NO ACTION",
-    });
-    await queryInterface.addConstraint("district_municipalities", {
-      fields: ["municipality_id"],
-      type: "foreign key",
-      name: "district_municipalities_municipality_id_fkey",
-      references: { table: "municipality", field: "id" },
-      onDelete: "NO ACTION",
-      onUpdate: "NO ACTION",
-    });
-  },
+  }
 
-  async down(queryInterface, Sequelize) {
-    await queryInterface.removeConstraint("district", "district_pkey", {});
-    await queryInterface.removeConstraint(
-      "district_municipalities",
-      "district_municipalities_pkey",
-      {},
-    );
-    await queryInterface.removeConstraint(
-      "district_municipalities",
-      "district_municipalities_district_id_fkey",
-      {},
-    );
-    await queryInterface.removeConstraint(
-      "district_municipalities",
-      "district_municipalities_municipality_id_fkey",
-      {},
-    );
-    await queryInterface.removeColumn("district", "id", {});
-    await queryInterface.removeColumn("district", "name", {});
-    await queryInterface.removeColumn("district_municipalities", "id", {});
-    await queryInterface.removeColumn(
-      "district_municipalities",
-      "district_id",
-      {},
-    );
-    await queryInterface.removeColumn(
-      "district_municipalities",
-      "municipality_id",
-      {},
-    );
-    await queryInterface.dropTable("district", {});
-    await queryInterface.dropTable("district_municipalities", {});
-  },
-};
+  static associate(models) {
+    this.belongsTo(models.District, {
+      foreignKey: "district_id",
+      onDelete: "CASCADE",
+      onUpdate: "CASCADE",
+    });
+
+    this.belongsTo(models.Municipality, {
+      foreignKey: "municipality_id",
+      onDelete: "CASCADE",
+      onUpdate: "CASCADE",
+    });
+  }
+}
+
+export default DistrictMunicipality;
